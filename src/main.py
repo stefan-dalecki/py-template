099767c5cda6e9f70d555a59c101d6c518de6875
Stefan Dalecki <daleckidev@gmail.com>
2025-07-23 07:34:09 -0600

    try to commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    Main function to execute the script.
+    This function is the entry point of the application.
+    """
+    print("Welcome to the Python Template Project!")
+
+
+if __name__ == "__main__":
+    main()
